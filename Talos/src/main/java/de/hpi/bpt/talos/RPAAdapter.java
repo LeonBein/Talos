6380233ebfa8eeb894fcdeb1b6e190e5f70bb657
Leon Bein <leon.bein@student.hpi.de>
2020-02-10 11:18:59 +0100

    Made task run timeout a bit more descriptive

--- Talos/src/main/java/de/hpi/bpt/talos/RPAAdapter.java
+++ Talos/src/main/java/de/hpi/bpt/talos/RPAAdapter.java
@@ -1,15 +1,19 @@
 package de.hpi.bpt.talos;
 
+import java.util.concurrent.TimeUnit;
+
 import de.hpi.bpt.talos.TalosCore.ProcessInputs;
 import de.hpi.bpt.talos.TalosCore.ProcessOutputs;
 
 public interface RPAAdapter<ProcessIdentifier> {
 	
+	static final long defaultTimeout = TimeUnit.MINUTES.toMillis(30);
+	
 	public default ProcessOutputs runProcess(String name, ProcessInputs processInputs) {
 		ProcessIdentifier jobId = startProcess(name, processInputs);
 		System.out.print("Waiting for process "+name+" to finish ... ");
 		try {
-			waitForTermination(jobId, 30 * 60 * 1000);
+			waitForTermination(jobId, defaultTimeout);
 		} catch (InterruptedException e) {
 			throw new RuntimeException("Execution of process "+name+" was interrupted unexpectedly: ", e);
 		}
