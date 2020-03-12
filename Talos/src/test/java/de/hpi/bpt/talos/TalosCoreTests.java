a469ac9679a6fa4220bbe6660acaff6f304da615
Leon Bein <leon.bein@student.hpi.de>
2020-03-12 16:20:32 +0100

    Added basic test infrastructure

---
+++ Talos/src/test/java/de/hpi/bpt/talos/TalosCoreTests.java
@@ -0,0 +1,28 @@
+package de.hpi.bpt.talos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class TalosCoreTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Tests whether the test environment is set up without errors
+	 */
+	@Test
+	void greenLightTest() {
+		assertTrue(true);
+	}
+
+}
